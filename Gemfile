b50cf54e5627a0add07f74895b3ec9200f01dec2
Karrock <clement.vachet@netapsys.fr>
2018-03-31 17:34:54 +0200

    [*]Add bundler to project

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem 'discordrb'
+
+# Specify your gem's dependencies in discordrb.gemspec
+gemspec name: 'discordrb'
+gemspec name: 'discordrb-webhooks', development_group: 'webhooks'
--- hots-bot.rb
+++ hots-bot.rb
@@ -1,10 +1,13 @@
 # This bot has various commands that show off CommandBot.
 
+require 'rubygems'
 require 'discordrb'
+
+# Bundler.require
 
 # Here we instantiate a `CommandBot` instead of a regular `Bot`, which has the functionality to add commands using the
 # `command` method. We have to set a `prefix` here, which will be the character that triggers command execution.
-bot = Discordrb::Commands::CommandBot.new token: 'NDA5ODIwODcyODA3ODA5MDM0.DWOUOg.mgGd-0fJLxEcwIbc-MTwX-YFlw0', prefix: '?'
+bot = Discordrb::Commands::CommandBot.new token: 'NDA5ODIwODcyODA3ODA5MDM0.DWOUOg.mgGd-0fJLxEcwIbc-MTwX-YFlw0', prefix: '~'
 
 bot.command :user do |event|
   # Commands send whatever is returned from the block to the channel. This allows for compact commands like this,
