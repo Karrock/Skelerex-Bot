44d7764330858c3805ffc05f369828ad8a35c830
Clément VACHET <clement.vachet@viacesi.fr>
2018-06-21 15:01:52 +0200

    Fix lowercase for arg and change command to hots

--- hots-bot.rb
+++ hots-bot.rb
@@ -20,25 +20,55 @@ bot.command :user do |event|
   event.user.name
 end
 
-bot.command(:icy, icy: 1) do |event, icy|
+bot.command(:hots, hots: 1) do |event, hots|
   icy_base = "https://www.icy-veins.com/heroes"
-  icy_veins = "https://www.icy-veins.com/heroes/#{icy}-build-guide"
 
-  if icy
-    file = open(icy_veins)
-    document = Nokogiri::HTML(file)
-    talents = document.at_css('div.heroes_tldr_talents')[0]
-    pp talents
+  if hots
+    hots = hots.downcase
+    icy_veins = "https://www.icy-veins.com/heroes/#{hots}-build-guide"
 
-    event << 'Hello ' + event.user.name + ' here your build for '+ icy.upcase + ' have fun ;)'
+    #file = open(icy_veins)
+    #document = Nokogiri::HTML(file)
+    #heroes_talents = document.css('div.heroes_tldr_talents').children
+
+    #talents = heroes_talents.gsub("\n","")
+    #talents = talents.gsub("?","")
+    #pp talents
+
+    ### Resonse
+    event << 'Hello ' + event.user.name + ' here is your build for '+ hots.upcase + ' have fun ;)'
+    #event << talents
     event << icy_veins
+
     # TODO handle 404 error
-    # 'Oops something went wrong maybe the given heroe : #{icy} is incorrect'
+    # 'Oops something went wrong maybe the given heroe : #{hots} is incorrect'
   else
     event << 'You don\'t send parameters with the command so here is the website.'
     event << icy_base
   end
 
 end
+
+bot.mention do |event|
+  event.user.pm(
+    'Yes my Lord, how can I help you ? Just type **~hots help** if you need help.'
+    )
+end
+
+### WIP need to check why bot couldn't talk
+bot.command(:rick) do |event|
+
+  channel = event.user.voice_channel
+  next "You're not in any voice channel!" unless channel
+  bot.voice_connect(channel)
+  event << "Connected to voice channel: #{channel.name}"
+
+  pp channel
+  voice_bot = event.voice
+  voice_bot.play_io('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+  bot.voice_destroy(channel)
+
+end
+
 
 bot.run
