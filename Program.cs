bee1eb8107660e4e7f1bcae40c49751db256b1b7
VACHET Clement <clement_msn67@hotmail.fr>
2021-11-30 20:42:56 +0100

    First commit to change project in C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace Skelerex_Bot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = File.ReadAllText(".env");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+    }
+}
