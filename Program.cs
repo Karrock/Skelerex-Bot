067d3a8e86bb082b31b2d264754fec633b8e68a2
VACHET Clement <clement_msn67@hotmail.fr>
2021-12-01 21:50:58 +0100

    Update code

--- Program.cs
+++ Program.cs
@@ -1,31 +1,101 @@
-﻿using System;
+﻿using System.IO;
+using System.Text;
 using System.Threading.Tasks;
-using Discord;
-using Discord.WebSocket;
-using Discord.Commands;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Skelerex_Bot
 {
     public class Program
     {
+        public readonly EventId BotEventId = new EventId(42, "Skelerex-basic");
+
+        public DiscordClient Client { get; set; }
         public static void Main(string[] args)
-            => new Program().MainAsync().GetAwaiter().GetResult();
-
-        private DiscordSocketClient _client;
-
-        public async Task MainAsync()
         {
-            _client = new DiscordSocketClient();
+            var prog = new Program();
+            prog.RunBotAsync().GetAwaiter().GetResult();
+        }
 
-            _client.Log += Log;
+        public async Task RunBotAsync()
+        {
+            // We load the config file
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
 
-            var token = File.ReadAllText(".env");
+            // Load values to our client's configuration
+            var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+            var cfg = new DiscordConfiguration
+            {
+                Token = cfgjson.Token,
+                TokenType = TokenType.Bot,
 
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug
+            };
 
-            // Block this task until the program is closed.
+            // then we want to instantiate our client
+            this.Client = new DiscordClient(cfg);
+
+            //Hook some events
+            this.Client.Ready += this.Client_Ready;
+            this.Client.GuildAvailable += this.Client_GuildAvailable;
+            this.Client.ClientErrored += this.Client_ClientError;
+
+            // finally, let's connect and log in
+            await this.Client.ConnectAsync();
+
+            // and this is to prevent premature quitting
             await Task.Delay(-1);
         }
+
+        private Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+        {
+            sender.Logger.LogInformation(BotEventId, "Client is ready to process events.");
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Client_GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
+        {
+            // let's log the name of the guild that was just
+            // sent to our client
+            sender.Logger.LogInformation(BotEventId, $"Guild available: {e.Guild.Name}");
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Client_ClientError(DiscordClient sender, ClientErrorEventArgs e)
+        {
+            // let's log the details of the error that just 
+            // occured in our client
+            sender.Logger.LogError(BotEventId, e.Exception, "Exception occured");
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
     }
+
+    // this structure will hold data from config.json
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("prefix")]
+        public string CommandPrefix { get; private set; }
+    }
+
 }
